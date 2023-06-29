262ce1f221b62aef5b7e31737893f3d4102c323c
indirasol <115805221+IndirasolGuevara@users.noreply.github.com>
2023-06-29 15:25:13 -0300

    indi

--- AngularBomberos/src/app/app-routing.module.ts
+++ AngularBomberos/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { TutorialComponent } from './pages/tutorial/tutorial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tutorial', component: TutorialComponent},
+  {path: 'login', component: LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
